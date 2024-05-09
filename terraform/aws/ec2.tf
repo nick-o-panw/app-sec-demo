8a06d9cd3dcd8f6b6f150c2913ae68e00eec3b81
Nick Ohri <78099152+nicko3185@users.noreply.github.com>
2024-05-08 20:31:06 -0400

    Update ec2.tf to test code scanning

--- terraform/aws/ec2.tf
+++ terraform/aws/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "web_host" {
-  # ec2 have plain text secrets in user 05082024
+  # ec2 have plain text secrets in user 050824
   ami           = "${var.ami}"
   instance_type = "t2.nano"
 
